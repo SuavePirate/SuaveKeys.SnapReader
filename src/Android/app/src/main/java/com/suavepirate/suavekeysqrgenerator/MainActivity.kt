c56b1abc234ed2a0cbfb8402cca507cb92928d1f
Alex Dunn <alexander.dunn@outlook.com>
2020-09-30 19:17:54 -0400

    updated main activity to send confirmation text, and updated lens to use script dynamic data and megamind

--- src/Android/app/src/main/java/com/suavepirate/suavekeysqrgenerator/MainActivity.kt
+++ src/Android/app/src/main/java/com/suavepirate/suavekeysqrgenerator/MainActivity.kt
@@ -3,12 +3,15 @@ package com.suavepirate.suavekeysqrgenerator
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.snapchat.kit.sdk.SnapCreative
+import com.snapchat.kit.sdk.creative.api.SnapCreativeKitCompletionCallback
+import com.snapchat.kit.sdk.creative.api.SnapCreativeKitSendError
 import com.snapchat.kit.sdk.creative.media.SnapLensLaunchData
 import com.snapchat.kit.sdk.creative.models.SnapLensContent
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SnapCreativeKitCompletionCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,29 +22,35 @@ class MainActivity : AppCompatActivity() {
             // Create a SnapLensContent object with LensID to be shared
             val snapLensContent = SnapLensContent("26b3d66e-4558-4081-9175-f51f7b4f6847")
 
-            // Optionally build SnapLensLaunchData, add to snapLensContent
+//            // Optionally build SnapLensLaunchData, add to snapLensContent
+//
+//            // Optionally build SnapLensLaunchData, add to snapLensContent
+            val launchData = SnapLensLaunchData.Builder()
+                .addStringKeyPair("test_text", "Hello from android")
+                .build()
 
-            // Optionally build SnapLensLaunchData, add to snapLensContent
-//            val launchData = SnapLensLaunchData.Builder()
-//                .addStringKeyPair("hair_color", "#b76e79")
-//                .addStringKeyPair("hint", "lens_hint_raise_your_eyebrows")
-//                .build()
-
-//            snapLensContent.snapLensLaunchData = launchData
-
-            // Optionally add an attachmentUrl and caption to the content
-
-            // Optionally add an attachmentUrl and caption to the content
-            snapLensContent.attachmentUrl = "https://snapchat.com"
-            snapLensContent.captionText = "Check out this Snap Kit Lens!"
+            snapLensContent.snapLensLaunchData = launchData
+//
+//            // Optionally add an attachmentUrl and caption to the content
+//
+//            // Optionally add an attachmentUrl and caption to the content
+//            snapLensContent.attachmentUrl = "https://snapchat.com"
+//            snapLensContent.captionText = "Check out this Snap Kit Lens!"
 
             // Use the SnapCreative API to share the snapLensContent
 
             // Use the SnapCreative API to share the snapLensContent
             val snapCreativeKitApi = SnapCreative.getApi(this)
-            snapCreativeKitApi.send(snapLensContent)
+            snapCreativeKitApi.sendWithCompletionHandler(snapLensContent, this)
 
             true
         }
+    }
+
+    override fun onSendSuccess() {
+    }
+
+    override fun onSendFailed(p0: SnapCreativeKitSendError?) {
+        // oh noooooo
     }
 }
